b4876f7ff869729fdbd0b028070b1fac12bd6782
Koncz Daniel <konczdaniel6@gmail.com>
2025-05-22 17:27:02 +0300

    First Commit

---
+++ WindowsFormsApp33/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp33
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(250, 47);
+            this.label10.MinimumSize = new System.Drawing.Size(0, 50);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(298, 50);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Calculator IMC";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(171, 145);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 36);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Barbat";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(487, 145);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(141, 36);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Femeie";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(181, 253);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            30000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 39);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(487, 253);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            30000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 39);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(106, 499);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(91, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 103);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calculeaza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(166, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Greutate in kg\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(468, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 26);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Inaltimea in cm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(382, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "sau";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(457, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(314, 111);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Arata masa ideala";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 878);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp33/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp33
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            button2.Enabled = false;        
+        }
+        private double CalculeazaIMC(double greutate, double inaltime)
+        {
+            inaltime = inaltime / 100;
+            return greutate / (inaltime * inaltime);
+        }
+       
+        private double CalculeazaMasaideala(double inaltime)
+        {
+            double greutate = 0;
+            
+            if (checkBox1.Checked==true)
+            {
+                greutate = 23;
+            }
+            else if (checkBox2.Checked==true)
+            {
+                greutate = 20;
+            }
+            
+            inaltime = inaltime / 100;
+            return greutate * (inaltime * inaltime);
+        }
+
+
+        //Interpretare IMC
+        private string InterpreteazaIMC(double imc)
+        { 
+            if (imc < 18.5)
+            {
+                return "Subponderal\n\nMasa corporală este prea mică.\nContactează-ți medicul pentru o consultație\nși pentru recomandări de analize și/sau modificarea dietei.";
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                return " Greutate Normala\n\nMasa corporală este normală.\nȚine o dietă bogată în fructe și legume,\nalături de un program zilnic mișcare pentru a menține o formă bună.";
+
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                return "Supraponderal\n\nMasa corporală este prea mare.\nFă-ți o consultație medicală sau revizuiește-ți stilul de viață";
+            }
+            else if (imc >= 30 && imc < 35)
+            {
+                return "Obezitate (gradul I)\n\nMasa corporală este prea mare,\niar indicatorul indică obezitate de grad 1.\nContactează-ți medicul, care va recomanda modificarea dietei și sport în fiecare zi.\nDacă vei lua măsuri din timp, eviți complicațiile aduse de obezitate\nși-ți păstrezi sănătatea pentru mai mult timp.";
+            }
+            else if (imc >= 35 && imc < 40)
+            {
+                return "Obezitate (gradul II)\n\nMasa corporală este mult prea mare,\niar indicatorul indică obezitate de grad 2.\nContactează-ți medicul, care va recomanda modificarea dietei și sport în fiecare zi.\nDacă vei lua măsuri din timp, eviți complicațiile aduse de obezitate\nși-ți păstrezi sănătatea pentru mai mult timp.";
+            }
+            else
+            {
+                return "Obezitate morbidă MASA CORPORALA MULT PREAM MARE.\n VA ROG SA CONSULTATI MEDICUL PENTRU BINELE DUMNEAVOASTRA.\nSANATATEA DUMNEAVOASTRA AR PUTEA FI IN PERICOL!! ";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double greutate = (double)numericUpDown1.Value;  //greutetea val
+            double inaltime = (double)numericUpDown2.Value;  //inaltimea val
+
+           
+            // Calculul IMC-ului
+            double imc = CalculeazaIMC(greutate, inaltime);
+
+            // Interpretarea IMC-ului
+            string interpretare = InterpreteazaIMC(imc);
+
+            // Afișarea rezultatului 
+            if (greutate == 0 && inaltime == 0)
+            {
+                label1.Text = $"Valoarea introdusa este invalida!";
+            }
+            else
+            {
+                label1.Text = $"IMC-ul dumneavoastra este: {imc:F3}";
+                label2.Text = $"Interpretare: {interpretare}";
+            }
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double greutate = (double)numericUpDown1.Value;  //greutetea val
+            double inaltime = (double)numericUpDown2.Value;  //inaltimea val
+
+            double mid = CalculeazaMasaideala(inaltime);
+
+            label1.Text = $"Greutatea ideala este: {mid}";
+            label2.Text = " ";
+
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = checkBox1.Checked;
+            button2.Enabled = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = checkBox2.Checked;
+            button2.Enabled = checkBox2.Checked;
+        }
+
+        
+    }
+}
